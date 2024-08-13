af9c07eaa249bc77a28b4c868a9f7844091a91bf
Erick Aguilar Gomez <65264358+ErickAguilarGomez@users.noreply.github.com>
2024-08-13 19:40:05 +0000

    Finalizando detalles del btn

--- src/js/utils/Items.jsx
+++ src/js/utils/Items.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from 'react';
+import './ItemDisplay.css';
 
-export const Items = ({index,task,handleDelete}) => {
+export const Items = ({ index, task, handleDelete }) => {
     return (
         <li key={index} className="list-group-item d-flex justify-content-between align-items-center list-group-item-action">
             <span>{task}</span>
-            <button onClick={() => { handleDelete(index) }} className="btn btn-danger">
+            <button
+                onClick={() => handleDelete(index)}
+                className="btn btn-danger hovertrue"
+            >
                 <i className="fa fa-trash" style={{ color: 'black' }}></i>
             </button>
         </li>
-    )
+    );
 }
