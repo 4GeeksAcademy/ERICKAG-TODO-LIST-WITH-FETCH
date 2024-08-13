632f5390bb05663ca81a562008ea0c1fe782385d
Erick Aguilar Gomez <65264358+ErickAguilarGomez@users.noreply.github.com>
2024-08-13 17:04:16 +0000

    TO DO LIST TERMINADO

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 const Home = () => {
+	const [input, setInput] = useState("")
+	const [tasks, setTasks] = useState([])
+
+	const handleInput = (e) => {
+		setInput(e.target.value)
+	}
+
+	const handleMakeTasks = () => {
+		if (input) {
+			setTasks([...tasks, input])
+			setInput("")
+		} else {
+			alert("Añade una tarea valida")
+		}
+
+	}
+
+	const handleDelete = (index) => {
+		const filterTasks = tasks.filter((_, idx) => tasks[idx] !== tasks[index])
+		setTasks(filterTasks)
+	}
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className="w-50 m-auto d-flex justify-content-evenly align-items-center" style={{ height: `10vh` }}>
+				<input type="text" value={input} onInput={handleInput} />
+				<button className="btn btn-primary" onClick={handleMakeTasks}>Crear tarea</button>
+			</div>
+
+			<ul className="w-50 mx-auto d-flex flex-column align-items-center list-group" style={{ height: '60vh' }}>
+				{tasks.length>0 ? tasks.map((task, index) => (
+						<div><li key={index} className="list-group-item">{task}</li>
+							<button onClick={() => { handleDelete(index) }}>Eliminar tarea</button>
+						</div>)) : <li className="list-group-item">Tarea vacía</li>}
+			</ul>
+
+
+		</>
 	);
 };
 
